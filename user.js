58041ee2ad8fe83c2875d0293e0631dde64e5097
RafidN <rafidn@outlook.com>
2023-06-06 14:41:50 -0500

    efficiency

--- user.js
+++ user.js
@@ -1,12 +1,21 @@
 const postListEl = document.querySelector(".post-list")
+const id = localStorage.getItem("id")
 
 async function onSearchChange(event) {
-    const id = event.target.value
+    const userId = event.target.value
 
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    renderPosts(userId);
+}
+
+async function renderPosts(userId) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`)
     const postsData = await posts.json();
-    
-    postListEl.innerHTML = postsData.map(post => `
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join("")
+
+}
+
+function postHTML(post) {
+    return `
     <div class="post">
         <div class="post__title">
         ${post.title}
@@ -14,24 +23,7 @@ async function onSearchChange(event) {
         <p class="post__body">
         ${post.body}
         </p>
-   </div>`).join("")
+   </div>`
 }
 
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-    
-    postListEl.innerHTML = postsData.map(post => `
-    <div class="post">
-        <div class="post__title">
-        ${post.title}
-        </div>
-        <p class="post__body">
-        ${post.body}
-        </p>
-   </div>`).join("")
-
-}
-
-main();
+renderPosts();
